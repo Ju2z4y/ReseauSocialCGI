695a65c483b2cfc2af14f4fccb33ec1d8af37635
julien <julien.griffault90@gmail.com>
2018-12-04 11:36:08 +0100

    Create DbDao.java

---
+++ src/main/java/com/reseau/social/cgi/db/DbDao.java
@@ -0,0 +1,13 @@
+package com.reseau.social.cgi.db;
+
+import com.reseau.social.cgi.model.Topic;
+
+public class DbDao {
+	
+	public Topic getTopicById(int id) {
+		Topic topic = new Topic();
+		
+		return topic;
+	}
+
+}
